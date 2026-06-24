94ac328f9191f9a9a06bc25ebbbee4011a418611
naima-uddin <toktikproject@gmail.com>
2026-06-24 12:28:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.set("trust proxy", 1);
 const ALLOWED_ORIGINS = new Set(
   (
     process.env.ALLOWED_ORIGINS ||
-    "http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173,https://budget-friendly-backend.vercel.app,https://smartproductbuy.com,https://budget-friendly-backend.onrender.com"
+    "http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173,https://budget-friendly-backend.vercel.app,http://pickob.com,https://budget-friendly-backend.onrender.com,https://smartproductbuy.com"
   )
     .split(",")
     .map((o) => o.trim())
